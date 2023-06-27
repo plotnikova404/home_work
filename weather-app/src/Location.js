5a06d9a9f689aab8c1a234a80c485404962aea6f
plotnikova404 <plotnikovaug@mail.ru>
2023-06-28 00:02:15 +0500

    create weather-app

---
+++ weather-app/src/Location.js
@@ -0,0 +1,12 @@
+import './Location.css';
+
+function Location() {
+    return (
+         <div className="Location"> 
+            <div className="City">Город</div>
+            <div className="Date">Дата</div>
+        </div>
+    );
+}
+
+export default Location;
